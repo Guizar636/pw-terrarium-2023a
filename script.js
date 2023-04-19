ff19c8dc0f34bebd4a19290168bd81966a651665
Guizar636 <124812297+Guizar636@users.noreply.github.com>
2023-04-19 19:06:03 +0000

    🏗 bulding dragging function

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let plant1 = document.getElementById("plant1")
-plant1.addEventListener("click", ()=>{
-    let name = prompt("Hola, ¿Cual es tu nombre?");
-    if (name != "")
-     alert(`${name} please, SAVE THE PLANET!!!🌵`);
-});
+//Haciendo que la planta 1 de mi documento sea arrastable
+dragElement(document.getElementById("plant1"));
+
+//Implementando la función drag element 
+function dragElement( terrariumElement){
+//Crear variables que controlaran las posiciones inciales y finales 
+let pos1 = 0, 
+pos2 = 0,
+pos3 = 0,
+pos4 = 0
+
+//Registrar  un evento 
+terrariumElement.onpointerdown = pointerDrag
+    
+function pointerDrag(event){
+    // Previene todo comportamiento
+        // que tenga por defecto
+        // el evento en cuestion en
+        // mi documento
+    event.preventDefault();
+    console.log("+ On pointer down");
+    }
+}
