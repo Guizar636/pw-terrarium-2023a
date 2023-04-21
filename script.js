860ba9584113f9e2fbd072db2e9cb60f9752b529
Guizar636 <124812297+Guizar636@users.noreply.github.com>
2023-04-21 18:04:21 +0000

    final 🏁

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 //Haciendo que la planta 1 de mi documento sea arrastable
 dragElement(document.getElementById("plant1"));
-
+dragElement(document.getElementById("plant2"));
+dragElement(document.getElementById("plant3"));
+dragElement(document.getElementById("plant4"));
+dragElement(document.getElementById("plant5"));
+dragElement(document.getElementById("plant6"));
+dragElement(document.getElementById("plant7"));
+dragElement(document.getElementById("plant8"));
+dragElement(document.getElementById("plant9"));
+dragElement(document.getElementById("plant10"));
+dragElement(document.getElementById("plant11"));
+dragElement(document.getElementById("plant12"));
+dragElement(document.getElementById("plant13"));
+dragElement(document.getElementById("plant14"));
 //Implementando la función drag element 
 function dragElement( terrariumElement){
 //Crear variables que controlaran las posiciones inciales y finales 
@@ -17,7 +29,44 @@ function pointerDrag(event){
         // que tenga por defecto
         // el evento en cuestion en
         // mi documento
-    event.preventDefault();
-    console.log("+ On pointer down");
+        event.preventDefault();
+        // Vamos a guardar la posicion inicial
+        // que se tiene al momento de hacer
+        // el pointerDrag
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+
+        // Registrando handler para evento
+        // de arrastre
+        document.onpointermove = elementDrag;
+        // Registrando handler para evento
+        // de liberación del boton
+        document.onpointerup = stopElementDrag;
+
+        //Creando las funciones
+        function elementDrag(event){
+            //Calculando la nueva posición 
+            pos1 = pos3 - event.clientX;
+            pos2 = pos4 - event.clientY;
+
+            //actualizamos posiciones originales
+            pos3 = event.clientX;
+            pos4 = event.clientY;
+
+            //mover la imagen con las posiciones calculadas
+            //de modo que la imagen quede en la nueva 
+            //posicion del mouse
+            terrariumElement.style.left =`${terrariumElement.offsetLeft - pos1}px`;
+            terrariumElement.style.top =`${terrariumElement.offsetTop - pos2}px`;
+        
+        }
+        function stopElementDrag(event){
+         //Eliminadno handler para evento de arrastre 
+        document.onpointermove = null;
+        //Eliminando handler para evento fr liberacion del boton 
+        document.onpointerup = null;
+
+
+        }
     }
 }
